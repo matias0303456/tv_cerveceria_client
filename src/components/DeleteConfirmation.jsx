59e2f6c02dfd6213b233b6eefce18ae56260ef55
Matias Ramos <matiasramos@gmail.com>
2023-09-22 15:15:35 -0300

    abm ingredientes terminado

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,10 +1,29 @@
-export function DeleteConfirmation({ t, id, method }) {
+import toast from "react-hot-toast";
+import { errorToast, successToast } from "../utils/toaster";
+
+export function DeleteConfirmation({ t, id, method, setter }) {
     return (
         <span>
-            Custom and <b>bold</b>
-            <button onClick={() => toast.dismiss(t.id)}>
-                Dismiss
-            </button>
+            <p>
+                ¿Eliminar este registro?
+            </p>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <button onClick={async () => {
+                    const { status, data } = await method(id)
+                    if (status === 200) {
+                        setter()
+                        toast.dismiss(t.id)
+                        successToast('Registro eliminado correctamente.')
+                    } else {
+                        errorToast(data.message)
+                    }
+                }}>
+                    Confirmar
+                </button>
+                <button onClick={() => toast.dismiss(t.id)}>
+                    Cancelar
+                </button>
+            </div>
         </span>
     )
 }
--- src/features/ingredients/IngredientCard.jsx
+++ src/features/ingredients/IngredientCard.jsx
@@ -1,8 +1,17 @@
+import { useContext } from 'react'
+import toast from 'react-hot-toast'
 import { AiFillDelete, AiFillEdit } from 'react-icons/ai'
 
-import { deleteConfirmation } from '../../utils/toaster'
+import { DeleteConfirmation } from '../../components/DeleteConfirmation'
+import { useIngredients } from './useIngredients'
+import { IngredientsContext } from './IngredientsProvider'
 
 export function IngredientCard({ ingredient, current, setCurrent, edit, setEdit }) {
+
+    const { deleteIngredient } = useIngredients()
+
+    const { setIngredients } = useContext(IngredientsContext)
+
     return (
         <div className="ingredientCard" style={{
             backgroundColor: (current.id !== ingredient.id || !edit) ? 'gold' : '#AD692A',
@@ -40,7 +49,16 @@ export function IngredientCard({ ingredient, current, setCurrent, edit, setEdit 
                         }}>
                             <AiFillEdit />
                         </span>
-                        <span className='actions' onClick={() => deleteConfirmation()}>
+                        <span className='actions' onClick={() => {
+                            toast(t => {
+                                return <DeleteConfirmation
+                                    t={t}
+                                    id={ingredient.id}
+                                    method={deleteIngredient}
+                                    setter={() => setIngredients(prev => [...prev.filter(item => item.id !== ingredient.id)])}
+                                />
+                            }, { position: 'bottom-right' })
+                        }}>
                             <AiFillDelete />
                         </span>
                     </> :
