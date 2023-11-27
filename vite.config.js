8abf42aa84bf11a0bf57245e478e268aac39c2b3
matias ramos <matiasramos261@gmail.com>
2023-11-27 15:15:23 -0300

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tv_cerveceria_client/',
+  base: '/asgardiana/',
   build: {
     outDir: './docs'
   }
